a683fc421fe03a56c0801c14ca7a21f16a90c021
Eileen <497015283@qq.com>
2021-10-08 03:02:52 -0400

    feat: Parsing Json & Msg format

--- cus.cpp
+++ cus.cpp
@@ -29,49 +29,80 @@ int main() {
   std::string s;
 
   if(curl) {
-  struct curl_slist *chunk = NULL;
-  chunk = curl_slist_append(chunk, "accept: application/vnd.api+json");
-  chunk = curl_slist_append(chunk, "x-api-key: 8ba0ca46476449399829b5304937dd19");
+    struct curl_slist *chunk = NULL;
+    chunk = curl_slist_append(chunk, "accept: application/vnd.api+json");
+    chunk = curl_slist_append(chunk, "x-api-key: 8ba0ca46476449399829b5304937dd19");
 
-  curl_easy_setopt(curl, CURLOPT_URL,  "https://api-v3.mbta.com/predictions?filter%5Bdirection_id%5D=1&filter%5Bstop%5D=place-bland&filter%5Broute%5D=Green-B&api_key=8ba0ca46476449399829b5304937dd19");
+    curl_easy_setopt(curl, CURLOPT_HTTPHEADER, chunk);
 
-  curl_easy_setopt(curl, CURLOPT_HTTPHEADER, chunk);
+    curl_easy_setopt(curl, CURLOPT_URL,  "https://api-v3.mbta.com/routes/Green-B?api_key=8ba0ca46476449399829b5304937dd19");
 
-  curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0L); //only for https
-  curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, 0L); //only for https
-  curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, CurlWrite_CallbackFunc_StdString);
-  curl_easy_setopt(curl, CURLOPT_WRITEDATA, &s);
-  //curl_easy_setopt (curl, CURLOPT_VERBOSE, 1L); //remove this to disable verbose output
+    // Prediction
+    //curl_easy_setopt(curl, CURLOPT_URL,  "https://api-v3.mbta.com/predictions?filter%5Bdirection_id%5D=1&filter%5Bstop%5D=place-bland&filter%5Broute%5D=Green-B&api_key=8ba0ca46476449399829b5304937dd19");
+
+    curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0L); //only for https
+    curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, 0L); //only for https
+    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, CurlWrite_CallbackFunc_StdString);
+    curl_easy_setopt(curl, CURLOPT_WRITEDATA, &s);
+    //curl_easy_setopt (curl, CURLOPT_VERBOSE, 1L); //remove this to disable verbose output
 
 
-  /* Perform the request, res will get the return code */
-  res = curl_easy_perform(curl);
-  /* Check for errors */
-  if(res != CURLE_OK) {
-    fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
-  }
+    /* Perform the request, res will get the return code */
+    res = curl_easy_perform(curl);
+    /* Check for errors */
+    if(res != CURLE_OK) {
+      fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
+    }
 
     /* always cleanup */
 
     curl_easy_cleanup(curl);
   }
 
-  std::cout<<s<<std::endl;
 
-  // auto j3 = json::parse(R"({"happy": true, "pi": 3.141})");
-  json j_string = s.c_str();
-  std::cout << j_string.dump(2) << std::endl;
+  auto j = json::parse(s.c_str());
+  auto destinations = j["data"]["attributes"]["direction_destinations"];
 
-  /*
   curl = curl_easy_init();
   if(curl) {
     curl_easy_setopt(curl, CURLOPT_URL, "https://hooks.slack.com/services/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");
-    std::string prefix = "{\"text\":\"";
-    std::string suffix = "\"}";
-    std::replace(s.begin(), s.end(), '"', '\'');
-    std::string data = prefix.append(s.c_str()).append(suffix);
-    std::cout<<"data: "<<data<<std::endl;
-    curl_easy_setopt(curl, CURLOPT_POSTFIELDS, data.c_str());
+
+    auto jslack = R"(
+      {
+        "blocks": [
+          {
+            "type": "section",
+            "fields": [
+              {
+                "type": "mrkdwn",
+                "text": "*Route:*\nGreen-B"
+              },
+              {
+                "type": "mrkdwn",
+                "text": "*Stop:*\nBlandford Street"
+              },
+              {
+                "type": "mrkdwn",
+                "text": "*Destination:*\nBoston College"
+              },
+              {
+                "type": "mrkdwn",
+                "text": "*Next:*\nUpcoming time"
+              }
+            ]
+          }
+        ]
+      }
+    )"_json;
+
+    std::string des_prefix = "*Destination:*\n";
+    std::string des = des_prefix.append(destinations[0]).append(", ").append(destinations[1]);
+
+    jslack["blocks"][0]["fields"][2]["text"] = des.c_str();
+
+    std::string slack_msg = jslack.dump();
+
+    curl_easy_setopt(curl, CURLOPT_POSTFIELDS, slack_msg.c_str());
  
     res = curl_easy_perform(curl);
     if(res != CURLE_OK)
@@ -81,6 +112,5 @@ int main() {
   }
 
   std::cout<< "Program finished!" << std::endl;
-  */
 }
 
