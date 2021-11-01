fb00f00bf54dde71acd5fbaa42b8b51cf4ec5cd9
Eileen <497015283@qq.com>
2021-11-01 16:42:30 -0400

    chore: Code clean

--- server/controllers/mbta_v1_Route.cc
+++ server/controllers/mbta_v1_Route.cc
@@ -3,23 +3,6 @@
 using namespace mbta::v1;
 //add definition of your processing function here
 
-void Route::getInfo(const HttpRequestPtr &req, std::function<void (const HttpResponsePtr &)> &&callback, std::string route, std::string stop, std::string direction) const {
-    LOG_DEBUG<<"Route "<<route<<"\n";
-    LOG_DEBUG<<"Stop "<<stop<<"\n";
-    LOG_DEBUG<<"Direction "<<direction<<"\n";
-
-    //验证token有效性等
-    //读数据库或缓存获取用户信息
-    Json::Value ret;
-    ret["route"]=route;
-    ret["stop"]=stop;
-    ret["direction"]=direction;
-    auto resp=HttpResponse::newHttpJsonResponse(ret);
-    callback(resp);
-}
-
-
-
 size_t CurlWrite_CallbackFunc_StdString(void *contents, size_t size, size_t nmemb, std::string *s) {
   size_t newLength = size*nmemb;
   try {
@@ -32,15 +15,7 @@ size_t CurlWrite_CallbackFunc_StdString(void *contents, size_t size, size_t nmem
   return newLength;
 }
 
-
-
-
-void Route::getBasicInfo(const HttpRequestPtr &req, std::function<void (const HttpResponsePtr &)> &&callback, std::string route) const {
-    LOG_DEBUG<<"Route "<<route<<"\n";
-
-
-
-
+std::string getData(std::string url) {
   CURL *curl;
   CURLcode res;
 
@@ -49,15 +24,15 @@ void Route::getBasicInfo(const HttpRequestPtr &req, std::function<void (const Ht
   curl = curl_easy_init();
   std::string s;
 
-
   if(curl) {
     struct curl_slist *chunk = NULL;
     chunk = curl_slist_append(chunk, "accept: application/vnd.api+json");
     chunk = curl_slist_append(chunk, "x-api-key: 8ba0ca46476449399829b5304937dd19");
 
     curl_easy_setopt(curl, CURLOPT_HTTPHEADER, chunk);
-
-    curl_easy_setopt(curl, CURLOPT_URL,  "https://api-v3.mbta.com/routes/Green-B?api_key=8ba0ca46476449399829b5304937dd19");
+    
+    curl_easy_setopt(curl, CURLOPT_URL,url.c_str());
+    //curl_easy_setopt(curl, CURLOPT_URL,  "https://api-v3.mbta.com/routes/Green-B?api_key=8ba0ca46476449399829b5304937dd19");
 
     // Prediction
     //curl_easy_setopt(curl, CURLOPT_URL,  "https://api-v3.mbta.com/predictions?filter%5Bdirection_id%5D=1&filter%5Bstop%5D=place-bland&filter%5Broute%5D=Green-B&api_key=8ba0ca46476449399829b5304937dd19");
@@ -69,27 +44,40 @@ void Route::getBasicInfo(const HttpRequestPtr &req, std::function<void (const Ht
     //curl_easy_setopt (curl, CURLOPT_VERBOSE, 1L); //remove this to disable verbose output
 
 
-    /* Perform the request, res will get the return code */
     res = curl_easy_perform(curl);
-    /* Check for errors */
     if(res != CURLE_OK) {
       fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
     }
 
-    /* always cleanup */
-
     curl_easy_cleanup(curl);
   }
+  return s;
+}
 
+void Route::getInfo(const HttpRequestPtr &req, std::function<void (const HttpResponsePtr &)> &&callback, std::string route, std::string stop, std::string direction) const {
+    LOG_DEBUG<<"Route "<<route<<"\n";
+    LOG_DEBUG<<"Stop "<<stop<<"\n";
+    LOG_DEBUG<<"Direction "<<direction<<"\n";
 
-  
-
-
-
+    // TODO: Dynamic query values based on request route
+    std::string url = "https://api-v3.mbta.com/predictions?filter%5Bdirection_id%5D=1&filter%5Bstop%5D=place-bland&filter%5Broute%5D=Green-B&api_key=8ba0ca46476449399829b5304937dd19";
 
     Json::Value ret;
     ret["route"]=route;
-    ret["data"]=s.c_str();
+    ret["stop"]=stop;
+    ret["direction"]=direction;
+    ret["data"] = getData(url).c_str();
+    auto resp=HttpResponse::newHttpJsonResponse(ret);
+    callback(resp);
+}
+
+void Route::getBasicInfo(const HttpRequestPtr &req, std::function<void (const HttpResponsePtr &)> &&callback, std::string route) const {
+    LOG_DEBUG<<"Route "<<route<<"\n";
+    std::string url = "https://api-v3.mbta.com/routes/Green-B?api_key=8ba0ca46476449399829b5304937dd19";
+ 
+    Json::Value ret;
+    ret["route"]=route;
+    ret["data"]=getData(url).c_str();
     auto resp=HttpResponse::newHttpJsonResponse(ret);
     callback(resp);
 }
--- server/debug.sh
+++
@@ -1,3 +0,0 @@
-docker build -f Dockerfile_debug -t debug-mbta-server .
-
-docker run -p 127.0.0.1:8888:80 -v /Users/eileen/Desktop/BU/C++/MBTA/server/controllers:/install/drogon/mbta/controllers -it debug-mbta-server:latest /bin/bash
