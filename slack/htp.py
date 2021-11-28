fe584826e6706ec7701978ccf822183ab892505a
Eileen <497015283@qq.com>
2021-11-27 23:46:55 -0500

    chore: Typo

--- slack/htp.py
+++
@@ -1,8 +0,0 @@
-import requests
-import json
-
-response = requests.get("http://localhost:8888/mbta/v1/route/Green-B/place-bland/0")
-data = response.json()["data"]
-pred = json.loads(data)["data"][0]["attributes"]
-print(pred["arrival_time"])
-
